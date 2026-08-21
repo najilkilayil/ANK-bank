574acad9531a7cf56bd79cfd1e0e8434426531e6
Najil <najiljan26@gmail.com>
2026-08-21 21:11:55 +0530

    Frame 2 : Added welcome animation after desktop warning!

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
 let loadingMain = document.getElementById("loading_main")
+let welcomeMain = document.getElementById("welcome_div")
 
 welcomeLoad()
 
 function welcomeLoad() {
     setTimeout(() => {
         loadingMain.id = "move_left_anim"
+        welcomeMain.style.display = "block"
+        welcomeMain.id = "move_right_anim"
         setTimeout(() => {
             loadingMain.style.opacity = "0"
             loadingMain.style.display = "none"
         }, 2500);
+        setTimeout(() => {
+            welcomeMain.style.display = "none"
+        }, 4000);
     }, 2000);
 }
