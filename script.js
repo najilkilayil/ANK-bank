a7e3127c0517921a9e499a2c8e1e8d9491eaccaa
Najil <najiljan26@gmail.com>
2026-08-22 08:01:24 +0530

    Frame 3 : Created fill form page and also collected everydatas and also created a function for random account number

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let loadingMain = document.getElementById("loading_main")
 let welcomeMain = document.getElementById("welcome_div")
+let fillFormMain = document.getElementById("form_main")
 
 welcomeLoad()
 
@@ -14,7 +15,39 @@ function welcomeLoad() {
             loadingMain.style.display = "none"
         }, 2500);
         setTimeout(() => {
-            welcomeMain.style.display = "none"
+            welcomeMain.id = "move_left_anim"
+            fillFormMain.id - "move_right_anim"
+            fillFormMain.style.display = "flex"
+            setTimeout(() => {
+                welcomeMain.style.display = "none"
+            }, 2000);
         }, 4000);
     }, 2000);
 }
+
+let accInput = document.getElementById("form_acc_number")
+
+function AccountNum() {
+    let digit1 = Math.floor(1000 + Math.random() * 20)
+    let digit2 = Math.floor(1000 + Math.random() * 9000)
+    
+    return `ANK-${digit1}-${digit2}`
+}
+
+accInput.value = AccountNum()
+
+let nameInput = document.getElementById("form_full_name")
+let balInput = document.getElementById("form_bal_amount")
+let passInput = document.getElementById("form_pin")
+let conPassInput = document.getElementById("form_pin_con")
+let fillFormSubBtn = document.getElementById("form_submit_btn")
+
+fillFormSubBtn.addEventListener("click", function() {
+    if (nameInput.value === "" || balInput.value === "" || passInput.value.length !== 4) {
+        alert("Fill all blanks!")
+    }
+
+    if (passInput.value !== conPassInput.value) {
+        alert("PIN is not match!")
+    }
+})
