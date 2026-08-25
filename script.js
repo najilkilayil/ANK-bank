a59886bdffb59547f7a60b5174b3f3c2307293cd
Najil <najiljan26@gmail.com>
2026-08-25 18:04:28 +0530

    Solved a bug

--- script.js
+++ script.js
@@ -42,9 +42,9 @@ function AccountNum() {
 }
 
 accInput.value = AccountNum()
-nameInput.value = "Robert John"
-passInput.value = "1111"
-conPassInput.value = "1111"
+// nameInput.value = "Robert John"
+// passInput.value = "1111"
+// conPassInput.value = "1111"
 
 
 
@@ -61,6 +61,7 @@ fillFormSubBtn.addEventListener("click", function (event) {
         return
     }
 
+    PIN = passInput.value
     showAccSummary()
 })
 let borderMain = document.getElementById("real_main")
